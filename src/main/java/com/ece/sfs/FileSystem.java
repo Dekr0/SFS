8f43e6bcefa5a77d684e47c284aaf5bed0529691
Dekr0 <dekr0.dk@gmail.com>
2023-01-28 23:25:02 -0700

    Add method for create and delete files and directories

--- src/main/java/com/ece/sfs/FileSystem.java
+++ src/main/java/com/ece/sfs/FileSystem.java
@@ -7,14 +7,91 @@ import java.util.Stack;
 public class FileSystem {
 
     // Directory Path
-    private final Stack<String> path = new Stack<>();
+    private Stack<String> path;
 
     // Map all files into a single table
-    private final HashMap<String, FileSystemComponent> absolutEntries = new HashMap<>();
+    private HashMap<String, FileSystemComponent> components;
 
     public FileSystem() {
-        absolutEntries.put("/", new Directory("", "/", Calendar.getInstance().getTime()));
+        components = new HashMap<>();
+        path = new Stack<>();
+    }
 
-        path.push("/");
+    public void createFile(String name) {
+        if (name == null || name.isEmpty()) {
+            throw new IllegalArgumentException("Invalid name");
+        }
+
+        String baseDir = getCurrentPath();
+        String absolutePath = Util.resolvePath(baseDir, name);
+        if (components.containsKey(absolutePath)) {
+            throw new IllegalArgumentException("File already exists");
+        } else {
+            components.put(absolutePath, new File(name, baseDir, 0L, Calendar.getInstance().getTime()));
+        }
+    }
+
+    public void deleteFile(String name) {
+        if (name == null || name.isEmpty()) {
+            throw new IllegalArgumentException("Invalid name");
+        }
+
+        String absolutePath = Util.resolvePath(getCurrentPath(), name);
+        if (components.containsKey(absolutePath)) {
+            components.remove(absolutePath);
+        } else {
+            throw new IllegalArgumentException("File does not exist");
+        }
+    }
+
+    public void createDirectory(String name) {
+        if (name == null || name.isEmpty()) {
+            throw new IllegalArgumentException("Invalid name");
+        }
+
+        String baseDir = getCurrentPath();
+        String absolutePath = Util.resolvePath(baseDir, name);
+        if (components.containsKey(absolutePath)) {
+            throw new IllegalArgumentException("Directory already exists");
+        } else {
+            components.put(absolutePath, new Directory(name, baseDir, Calendar.getInstance().getTime()));
+        }
+    }
+
+    public void deleteDirectory(String name) {
+        if (name == null || name.isEmpty()) {
+            throw new IllegalArgumentException("Invalid name");
+        }
+
+        String absolutePath = Util.resolvePath(getCurrentPath(), name);
+        if (components.containsKey(absolutePath)) {
+            components.remove(absolutePath);
+        } else {
+            throw new IllegalArgumentException("Directory does not exist");
+        }
+    }
+
+    public void rename(String name, String newName) {
+        if (name == null || name.isEmpty()) {
+            throw new IllegalArgumentException("Invalid name");
+        }
+
+        if (newName == null || newName.isEmpty()) {
+            throw new IllegalArgumentException("Invalid new name");
+        }
+
+    }
+
+    public String getCurrentPath() {
+        StringBuilder currentPath = new StringBuilder();
+        for (String s : path) {
+            if (s.compareTo("/") == 0) {
+                currentPath = new StringBuilder(s);
+            } else {
+                currentPath.append("/").append(s);
+            }
+        }
+
+        return currentPath.toString();
     }
 }
