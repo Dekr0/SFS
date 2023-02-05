f30e272e9d45c8d495305fb5ab2602869e559d4c
Dekr0 <dekr0.dk@gmail.com>
2023-02-04 23:28:21 -0700

    Remove non-required methods; Grouping methods; Add methods for getting parent directory and encrypted name.

--- src/main/java/com/ece/sfs/Directory.java
+++ src/main/java/com/ece/sfs/Directory.java
@@ -4,68 +4,76 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.UUID;
 
-public class Directory extends FileSystemComponent {
+public class Directory extends Component {
 
-    private String absolutePath = "";
-    private String baseDir = "";
-    private String name = "";
+    private AccessRightList accessRightList;
+    private Directory parent = null;
     private Date lastModifiedDate;
+    private String name = "";
+    private final UUID uuid;
 
-    private UUID uuid;
-
-    private HashMap<String, FileSystemComponent> entries;
+    private final HashMap<String, Component> components;
 
     // TODO: Implement File Navigation
     // TODO: Implement Group and User Access for directory
 
-    public Directory(String baseDir, String name, Date lastModifiedDate) {
+    public Directory(String name, Directory parent, Date lastModifiedDate) {
         uuid = UUID.randomUUID();
-        entries = new HashMap<>();
+        components = new HashMap<>();
         setName(name);
-        setBaseDir(baseDir);
+        setParent(parent);
         setDate(lastModifiedDate);
-        setAbsolutePath(Util.resolvePath(baseDir, name));
     }
 
     @Override
-    public Long getSize() {
-        Long size = 0L;
-        for (FileSystemComponent fileSystemComponent : entries.values()) {
-            size += fileSystemComponent.getSize();
+    public void addComponent(Component component) {
+        if (component == null) {
+            throw new IllegalArgumentException("Adding a null object into the directory");
         }
 
-        return size;
+        components.put(component.getName(), component);
     }
 
     @Override
-    public String getAbsolutePath() {
-        return absolutePath;
+    public Component getComponent(String name) {
+        if (hasComponent(name)) {
+            return components.get(name);
+        }
+
+        throw new IllegalArgumentException("No such file or directory: " + name);
     }
 
-    public void setAbsolutePath(String absolutePath) {
-        if (absolutePath != null) {
-            this.absolutePath = absolutePath;
-        } else {
-            throw new IllegalArgumentException("Invalid absolute path");
+    @Override
+    public boolean hasComponent(String name) {
+        return components.containsKey(name);
+    }
+
+    @Override
+    public void removeComponent(String name) {
+        if (components.remove(name) == null) {
+            throw new IllegalArgumentException(
+                    "File or Directory" + name + "does not exist");
         }
     }
 
     @Override
-    public String getBaseDir() {
-        return baseDir;
+    public Component getParent() {
+        return parent;
     }
 
     @Override
-    public void setBaseDir(String baseDir) {
+    public void setParent(Directory parent) {
         if (name.compareTo("/") == 0) {
-            if (baseDir.isEmpty()) {
-                return;
-            } else {
-                throw new IllegalArgumentException("Invalid base directory");
+            if (parent != null) {
+                throw new IllegalArgumentException("/ directory does not have a parent directory");
             }
-        }
+        } else {
+            if (parent == null) {
+                throw new IllegalArgumentException("Parent directory cannot be null");
+            }
 
-        this.baseDir = baseDir;
+            this.parent = parent;
+        }
     }
 
     @Override
@@ -95,27 +103,6 @@ public class Directory extends FileSystemComponent {
 
         if (date instanceof Date) {
             this.lastModifiedDate = (Date) date;
-        }
-    }
-
-    @Override
-    public void add(FileSystemComponent fileSystemComponent) {
-        if (fileSystemComponent == null) {
-            throw new IllegalArgumentException("Invalid file system component");
-        }
-
-        entries.put(fileSystemComponent.getName(), fileSystemComponent);
-    }
-
-    @Override
-    public void remove(FileSystemComponent fileSystemComponent) {
-        if (fileSystemComponent == null) {
-            throw new IllegalArgumentException("Invalid file system component");
-        }
-
-        if (entries.remove(fileSystemComponent.getName()) == null) {
-            throw new IllegalArgumentException("File or Directory" +
-                    fileSystemComponent.getName() + "does not exist");
         }
     }
 
