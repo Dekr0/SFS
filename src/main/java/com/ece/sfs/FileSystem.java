5a1b45a5f56314e41bb37ea46173f170c690ada0
Dekr0 <dekr0.dk@gmail.com>
2023-02-04 23:29:24 -0700

    Implement change directory, and create, rename, and delete file / directory

--- src/main/java/com/ece/sfs/FileSystem.java
+++ src/main/java/com/ece/sfs/FileSystem.java
@@ -1,97 +1,115 @@
 package com.ece.sfs;
 
 import java.util.Calendar;
-import java.util.HashMap;
-import java.util.Stack;
+
 
 public class FileSystem {
 
-    // Directory Path
-    private Stack<String> path;
-
-    // Map all files into a single table
-    private HashMap<String, FileSystemComponent> components;
-
-    public FileSystem() {
-        components = new HashMap<>();
-        path = new Stack<>();
+    public enum FileType {
+        FILE, DIR
     }
 
-    public void createFile(String name) {
-        if (name == null || name.isEmpty()) {
-            throw new IllegalArgumentException("Invalid name");
+    private final Directory root;
+    private Directory currentDirectory;
+
+
+    public FileSystem(Directory root, String usr) {
+        this.root = root;
+
+        currentDirectory = (Directory) root.getComponent("home");
+
+        if (!currentDirectory.hasComponent(usr)) {
+            currentDirectory.addComponent(
+                    new Directory(
+                            usr,
+                            currentDirectory,
+                            Calendar.getInstance().getTime()
+                    )
+            );
         }
 
-        String baseDir = getCurrentPath();
-        String absolutePath = Util.resolvePath(baseDir, name);
-        if (components.containsKey(absolutePath)) {
-            throw new IllegalArgumentException("File already exists");
-        } else {
-            components.put(absolutePath, new File(name, baseDir, 0L, Calendar.getInstance().getTime()));
-        }
+        currentDirectory = (Directory) currentDirectory.getComponent(usr);
     }
 
-    public void deleteFile(String name) {
-        if (name == null || name.isEmpty()) {
-            throw new IllegalArgumentException("Invalid name");
-        }
-
-        String absolutePath = Util.resolvePath(getCurrentPath(), name);
-        if (components.containsKey(absolutePath)) {
-            components.remove(absolutePath);
-        } else {
-            throw new IllegalArgumentException("File does not exist");
-        }
-    }
-
-    public void createDirectory(String name) {
-        if (name == null || name.isEmpty()) {
-            throw new IllegalArgumentException("Invalid name");
-        }
-
-        String baseDir = getCurrentPath();
-        String absolutePath = Util.resolvePath(baseDir, name);
-        if (components.containsKey(absolutePath)) {
-            throw new IllegalArgumentException("Directory already exists");
-        } else {
-            components.put(absolutePath, new Directory(name, baseDir, Calendar.getInstance().getTime()));
+    public void changeDirectory(String name) {
+        if (name != null && !name.isEmpty()) {
+            try {
+                currentDirectory = (Directory) currentDirectory.getComponent(name);
+            } catch (IllegalArgumentException e) {
+                System.err.println(e.getMessage());
+            } catch (ClassCastException e) {
+                System.err.println(name + " is not a directory");
+            }
         }
     }
 
-    public void deleteDirectory(String name) {
-        if (name == null || name.isEmpty()) {
-            throw new IllegalArgumentException("Invalid name");
+    public void changePrevDirectory() {
+        if (currentDirectory.getParent() != null) {
+            currentDirectory = (Directory) currentDirectory.getParent();
         }
-
-        String absolutePath = Util.resolvePath(getCurrentPath(), name);
-        if (components.containsKey(absolutePath)) {
-            components.remove(absolutePath);
-        } else {
-            throw new IllegalArgumentException("Directory does not exist");
-        }
-    }
-
-    public void rename(String name, String newName) {
-        if (name == null || name.isEmpty()) {
-            throw new IllegalArgumentException("Invalid name");
-        }
-
-        if (newName == null || newName.isEmpty()) {
-            throw new IllegalArgumentException("Invalid new name");
-        }
-
     }
 
     public String getCurrentPath() {
         StringBuilder currentPath = new StringBuilder();
-        for (String s : path) {
-            if (s.compareTo("/") == 0) {
-                currentPath = new StringBuilder(s);
-            } else {
-                currentPath.append("/").append(s);
-            }
+
+        Directory current = currentDirectory;
+        while (current != root) {
+            currentPath.insert(0, "/" + current.getName());
+            current = (Directory) current.getParent();
         }
 
         return currentPath.toString();
+    }
+
+    public void createComponent(String name, FileType type) {
+        if (name == null || name.isEmpty()) {
+            System.err.println("Name cannot be empty");
+        } else {
+            try {
+                if (type == FileType.FILE) {
+                    currentDirectory.addComponent(
+                            new File(
+                                    name,
+                                    currentDirectory,
+                                    Calendar.getInstance().getTime()
+                            )
+                    );
+                } else if (type == FileType.DIR) {
+                    currentDirectory.addComponent(
+                            new Directory(
+                                    name,
+                                    currentDirectory,
+                                    Calendar.getInstance().getTime()
+                            )
+                    );
+                }
+            } catch (IllegalArgumentException e) {
+                System.err.println(e.getMessage());
+            }
+        }
+    }
+
+    public void deleteComponent(String name) {
+        if (name == null || name.isEmpty()) {
+            System.err.println("Name cannot be empty");
+        } else {
+            try {
+                currentDirectory.removeComponent(name);
+            } catch (IllegalArgumentException e) {
+                System.err.println(e.getMessage());
+            }
+        }
+    }
+
+    public void renameComponent(String oldName, String newName) {
+        try {
+            Component component = currentDirectory.getComponent(oldName);
+            component.setName(newName);
+
+            currentDirectory.removeComponent(oldName);
+            currentDirectory.addComponent(component);
+        } catch (IllegalArgumentException e) {
+            System.err.println(e.getMessage());
+        }
     }
 }
