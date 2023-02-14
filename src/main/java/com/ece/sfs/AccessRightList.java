daa74d2979edece9007e61b6e327d48c472036c3
Dekr0 <dekr0.dk@gmail.com>
2023-02-14 04:31:04 -0700

    TODO: Include user group

--- src/main/java/com/ece/sfs/AccessRightList.java
+++ src/main/java/com/ece/sfs/AccessRightList.java
@@ -1,4 +1,30 @@
 package com.ece.sfs;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
+
 public class AccessRightList {
+    private final HashMap<String, ArrayList<AccessRight>> accessRightList;
+
+    public AccessRightList(HashMap<String, ArrayList<AccessRight>> accessRightList) {
+        this.accessRightList = accessRightList;
+    }
+
+    public void addAccessRight(String username, ArrayList<AccessRight> newAccessRights) {
+        if (!accessRightList.containsKey(username)) {
+            accessRightList.put(username, new ArrayList<>(newAccessRights));
+        } else {
+            for (AccessRight newAccessRight : newAccessRights) {
+                if (!accessRightList.get(username).contains(newAccessRight)) {
+                    accessRightList.get(username).add(newAccessRight);
+                }
+            }
+        }
+    }
+
+    public boolean validAccessRight(String username, AccessRight accessRight) {
+        return accessRightList.containsKey(username) &&
+                accessRightList.get(username).contains(accessRight);
+    }
 }
