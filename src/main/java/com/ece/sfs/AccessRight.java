97763366f2728ee52823a9015547a483194b164c
Dekr0 <dekr0.dk@gmail.com>
2023-02-14 04:30:26 -0700

    Add static method for creation of default access right and add root privilege to every component

--- src/main/java/com/ece/sfs/AccessRight.java
+++ src/main/java/com/ece/sfs/AccessRight.java
@@ -1,8 +1,20 @@
 package com.ece.sfs;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 public enum AccessRight {
     EXECUTE,
     OWN,
     READ,
-    WRITE
+    WRITE;
+
+    public static ArrayList<AccessRight> defaultAR() {
+        return new ArrayList<>(List.of(EXECUTE, OWN, READ, WRITE));
+    }
+
+    public static Map<String, ArrayList<AccessRight>> rootAR() {
+        return Map.of("root", defaultAR());
+    }
 }
