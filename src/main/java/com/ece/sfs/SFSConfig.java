5b1d019c9acbb53ceb2ab651e726dc44a69c33a0
Dekr0 <dekr0.dk@gmail.com>
2023-02-21 05:16:26 -0700

    New injection

--- src/main/java/com/ece/sfs/SFSConfig.java
+++ src/main/java/com/ece/sfs/SFSConfig.java
@@ -1,9 +1,12 @@
 package com.ece.sfs;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
 import java.util.*;
 
@@ -16,7 +19,6 @@ public class SFSConfig {
     public Directory root() {
         Directory root = new Directory(
                 "/",
-                new AccessRightList(new HashMap<>()),
                 null,
                 Calendar.getInstance().getTime(),
                 UUID.randomUUID(),
@@ -26,7 +28,6 @@ public class SFSConfig {
         root.addComponent(
                 new Directory(
                         "home",
-                        new AccessRightList(new HashMap<>()),
                         root,
                         Calendar.getInstance().getTime(),
                         UUID.randomUUID(),
@@ -36,4 +37,61 @@ public class SFSConfig {
 
         return root;
     }
+
+    @Bean
+    @Scope("singleton")
+    @Qualifier("INTERNAL")
+    public GrantedAuthority INTERNAL() {
+        return new SimpleGrantedAuthority("INTERNAL");
+    }
+
+
+    @Bean
+    @Scope("singleton")
+    @Qualifier("EXTERNAL")
+    public GrantedAuthority EXTERNAL() {
+        return new SimpleGrantedAuthority("EXTERNAL");
+    }
+
+    @Bean
+    @Scope("singleton")
+    public UserGroupManager userGroupManager() {
+        UserGroupManager manager = new UserGroupManager(new ArrayList<>());
+
+        manager.createGroup("Users", Collections.singletonList(INTERNAL()));
+        manager.createGroup("Admins", Collections.singletonList(INTERNAL()));
+
+        return manager;
+    }
+
+    @Bean
+    @Scope("singleton")
+    public AccessManager accessRightListManager(Directory root, UserGroupManager userGroupManager) {
+        AccessManager manager = new AccessManager(userGroupManager);
+
+        manager.addAccessRightList(
+                root.getUUID(),
+                new AccessRightList(
+                        AccessRight.defaultAR("root"),
+                        AccessRight.defaultRead("Admins")
+                )
+        );
+
+        manager.addAccessRightList(
+                root.getComponent("home").getUUID(),
+                new AccessRightList(
+                        AccessRight.defaultAR("root"),
+                        AccessRight.defaultRead("Users")
+                )
+        );
+
+        manager.addGroupAccessRight(
+                "Admin",
+                root.getComponent("home").getUUID(),
+                AccessRight.defaultAR()
+        );
+
+        return manager;
+    }
+
 }
