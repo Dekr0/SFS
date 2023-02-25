ff55f849579e58b11f387335155fc59ca2a328fe
Dekr0 <dekr0.dk@gmail.com>
2023-02-24 19:24:10 -0700

    Convert test case to suit with Either Construct

--- src/main/java/com/ece/sfs/SFSApplication.java
+++ src/main/java/com/ece/sfs/SFSApplication.java
@@ -1,6 +1,5 @@
 package com.ece.sfs;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,19 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SFSApplication {
 
-	private final Directory root;
-
-	@Autowired
-	public SFSApplication(Directory root) {
-		this.root = root;
-	}
-
-	private void loop() {
-
-	}
-
 	public static void main(String[] args) {
 		SpringApplication.run(SFSApplication.class, args);
 	}
-
 }
--- src/test/java/com/ece/sfs/SFSApplicationTests.java
+++ src/test/java/com/ece/sfs/SFSApplicationTests.java
@@ -1,5 +1,10 @@
 package com.ece.sfs;
 
+import com.ece.sfs.access.AccessManager;
+import com.ece.sfs.group.UserGroupManager;
+import com.ece.sfs.core.Directory;
+import com.ece.sfs.core.FileSystem;
+import io.vavr.control.Either;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -19,39 +24,39 @@ class SFSApplicationTests {
 	private final AccessManager accessManager;
 	private final UserGroupManager userGroupManager;
 	private final Directory root;
-	private final GrantedAuthority INTERNAL;
-	private final GrantedAuthority EXTERNAL;
+	private final GrantedAuthority adminAuthority;
+	private final GrantedAuthority userAuthority;
 
 	@Autowired
 	SFSApplicationTests(
 			AccessManager accessManager,
 			UserGroupManager userGroupManager,
 			Directory root,
-			GrantedAuthority INTERNAL,
-			GrantedAuthority EXTERNAL
+			GrantedAuthority adminAuthority,
+			GrantedAuthority userAuthority
 	) {
 		this.accessManager = accessManager;
 		this.userGroupManager = userGroupManager;
 		this.root = root;
-		this.INTERNAL = INTERNAL;
-		this.EXTERNAL = EXTERNAL;
+		this.adminAuthority = adminAuthority;
+		this.userAuthority = userAuthority;
 	}
 
 	@Test
 	void contextLoads() {
 		assertEquals("/", root.getName());
-		assertEquals("INTERNAL", INTERNAL.getAuthority());
-		assertEquals("EXTERNAL", EXTERNAL.getAuthority());
+		assertEquals("Admin", adminAuthority.getAuthority());
+		assertEquals("User", userAuthority.getAuthority());
 	}
 
 	@Test
 	void testPrivilege() {
-		User userA = new User("userA", "password", Collections.singletonList(INTERNAL));
-		User userB = new User("userB", "password", Collections.singletonList(INTERNAL));
-		User userC = new User("userC", "password", Collections.singletonList(EXTERNAL));
+		User userA = new User("userA", "password", Collections.singletonList(adminAuthority));
+		User userB = new User("userB", "password", Collections.singletonList(adminAuthority));
+		User userC = new User("userC", "password", Collections.singletonList(userAuthority));
 
-		userGroupManager.createGroup("Group 1", Collections.singletonList(INTERNAL));
-		userGroupManager.createGroup("Group 2", Collections.singletonList(INTERNAL));
+		userGroupManager.createGroup("Group 1", Collections.singletonList(adminAuthority));
+		userGroupManager.createGroup("Group 2", Collections.singletonList(adminAuthority));
 
 		userGroupManager.addUserToGroup(userA.getUsername(), "Users");
 		userGroupManager.addUserToGroup(userB.getUsername(), "Users");
@@ -65,31 +70,82 @@ class SFSApplicationTests {
 		session1.loginHome("Group 1", userA.getUsername());
 		session2.loginHome("Group 1", userB.getUsername());
 
-		assertTrue(session1.createComponent("a.txt", FileSystem.FileType.FILE));
-		assertTrue(session1.createComponent("b", FileSystem.FileType.DIR));
-		assertTrue(session1.changeDirectory("b"));
-		assertTrue(session1.changeDirectory(".."));
-		assertTrue(session1.changeDirectory(".."));
-		assertFalse(session1.changeDirectory(".."));
+		Either<Boolean, String> r;
 
-		assertTrue(session2.changeDirectory(".."));
-		assertTrue(session2.changeDirectory("userA"));
-		assertTrue(session2.changeDirectory("b"));
-		assertTrue(session2.changeDirectory(".."));
-		assertFalse(session2.renameComponent("a.txt", "c.txt"));
-		assertFalse(session2.deleteComponent("a.txt"));
-		assertFalse(session2.createComponent("b", FileSystem.FileType.DIR));
-		assertFalse(session2.createComponent("c.txt", FileSystem.FileType.FILE));
+		r = session1.createComponent("a.txt", FileSystem.FileType.FILE);
+		assertTrue(r.isLeft() && r.getLeft());
+		r = session1.createComponent("b", FileSystem.FileType.DIR);
+		assertTrue(r.isLeft() && r.getLeft());
+
+		r = session1.changeDirectory("b");
+		assertTrue(r.isLeft() && r.getLeft());
+		assertEquals("/home/userA/b", session1.getCurrentPath());
+
+		r = session1.changeDirectory("..");
+		assertTrue(r.isLeft() && r.getLeft());
+		assertEquals("/home/userA", session1.getCurrentPath());
+
+		r = session1.changeDirectory("..");
+		assertTrue(r.isLeft() && r.getLeft());
+		assertEquals("/home", session1.getCurrentPath());
+
+		r = session1.changeDirectory("..");
+		assertTrue(r.isRight() &&
+				r.get().compareTo("You do not have access rights to this directory") == 0);
+		assertEquals("/home", session1.getCurrentPath());
+
+		r = session1.changeDirectory("userA");
+		assertTrue(r.isLeft() && r.getLeft());
+		assertEquals("/home/userA", session1.getCurrentPath());
+
+
+		r = session2.changeDirectory("..");
+		assertTrue(r.isLeft() && r.getLeft());
+		assertEquals("/home", session2.getCurrentPath());
+
+		r = session2.changeDirectory("userA");
+		assertTrue(r.isLeft() && r.getLeft());
+		assertEquals("/home/userA", session2.getCurrentPath());
+
+		r = session2.changeDirectory("b");
+		assertTrue(r.isLeft() && r.getLeft());
+		assertEquals("/home/userA/b", session2.getCurrentPath());
+
+		r = session2.changeDirectory("..");
+		assertTrue(r.isLeft() && r.getLeft());
+
+		r = session2.renameComponent("a.txt", "c.txt");
+		assertTrue(r.isRight() &&
+				r.get().compareTo("You do not have permission to rename " + "a.txt") == 0);
+
+		r = session2.deleteComponent("a.txt");
+		assertTrue(r.isRight() &&
+				r.get().compareTo("You do not have permission to delete this component") == 0);
+
+		r = session2.createComponent("b", FileSystem.FileType.DIR);
+		assertTrue(r.isRight() &&
+				r.get().compareTo("Component " + "b" + " already exists") == 0);
+
+		r = session2.createComponent("c.txt", FileSystem.FileType.FILE);
+		assertTrue(r.isRight() &&
+				r.get().compareTo("You do not have permission to create a component in this directory") == 0);
+
+		session2.ls();
+
+		r = session1.createComponent("c.txt", FileSystem.FileType.FILE);
+		assertTrue(r.isLeft() && r.getLeft());
+
+		session2.ls();
 	}
 
 	@Test
 	void testCreateRemoveUserGroup() {
-		assertDoesNotThrow(() -> userGroupManager.createGroup("Group 1", List.of(INTERNAL)));
-		assertThrows(IllegalArgumentException.class, () -> userGroupManager.createGroup("Group 1", List.of(INTERNAL)));
+		assertDoesNotThrow(() -> userGroupManager.createGroup("Group 1", List.of(adminAuthority)));
+		assertThrows(IllegalArgumentException.class, () -> userGroupManager.createGroup("Group 1", List.of(adminAuthority)));
 		assertDoesNotThrow(() -> userGroupManager.deleteGroup("Group 1"));
 		assertThrows(IllegalArgumentException.class, () -> userGroupManager.deleteGroup("Group 1"));
 
-		assertDoesNotThrow(() -> userGroupManager.createGroup("Group 2", List.of(INTERNAL)));
+		assertDoesNotThrow(() -> userGroupManager.createGroup("Group 2", List.of(adminAuthority)));
 
 		assertDoesNotThrow(() -> userGroupManager.renameGroup("Group 2", "Group 3"));
 
@@ -103,17 +159,17 @@ class SFSApplicationTests {
 	@Test
 	void testAddRemoveAuthority() {
 		assertDoesNotThrow(() -> userGroupManager.createGroup("Group 1", new ArrayList<>()));
-		assertDoesNotThrow(() -> userGroupManager.addGroupAuthority("Group 1", INTERNAL));
-		assertThrows(IllegalArgumentException.class, () -> userGroupManager.addGroupAuthority("Group 1", INTERNAL));
-		assertDoesNotThrow(() -> userGroupManager.removeGroupAuthority("Group 1", INTERNAL));
-		assertThrows(IllegalArgumentException.class, () -> userGroupManager.removeGroupAuthority("Group 1", INTERNAL));
+		assertDoesNotThrow(() -> userGroupManager.addGroupAuthority("Group 1", adminAuthority));
+		assertThrows(IllegalArgumentException.class, () -> userGroupManager.addGroupAuthority("Group 1", adminAuthority));
+		assertDoesNotThrow(() -> userGroupManager.removeGroupAuthority("Group 1", adminAuthority));
+		assertThrows(IllegalArgumentException.class, () -> userGroupManager.removeGroupAuthority("Group 1", adminAuthority));
 	}
 
 	@Test
 	void testAddRemoveUser() {
-		User userA = new User("User A", "password", new ArrayList<>(List.of(INTERNAL)));
+		User userA = new User("User A", "password", new ArrayList<>(List.of(adminAuthority)));
 
-		assertDoesNotThrow(() -> userGroupManager.createGroup("Group 1", new ArrayList<>(List.of(INTERNAL))));
+		assertDoesNotThrow(() -> userGroupManager.createGroup("Group 1", new ArrayList<>(List.of(adminAuthority))));
 		assertDoesNotThrow(() -> userGroupManager.addUserToGroup(userA.getUsername(), "Group 1"));
 
 		assertThrows(IllegalArgumentException.class, () -> userGroupManager.addUserToGroup(userA.getUsername(), "Group 1"));
