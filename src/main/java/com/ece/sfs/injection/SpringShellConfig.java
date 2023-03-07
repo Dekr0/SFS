f791dbc381ace73332f9c4b0c108824e2d5b9a9b
Dekr0 <54260982+Dekr0@users.noreply.github.com>
2023-03-07 15:45:54 -0700

    Fixing prompt issues

--- src/main/java/com/ece/sfs/injection/SpringShellConfig.java
+++ src/main/java/com/ece/sfs/injection/SpringShellConfig.java
@@ -3,6 +3,7 @@ package com.ece.sfs.injection;
 
 import com.ece.sfs.prompt.ShellPrompt;
 import org.jline.terminal.Terminal;
+import org.jline.terminal.TerminalBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,7 +11,7 @@ import org.springframework.context.annotation.Configuration;
 public class SpringShellConfig {
 
     @Bean
-    public ShellPrompt shellPrompt() throws IOException {
-        return new ShellPrompt(TerminalBuilder.system(false).build());
+    public ShellPrompt shellPrompt() throws Exception {
+        return new ShellPrompt(TerminalBuilder.builder().system(false).build());
     }
 }
